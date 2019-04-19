c0e40a9ae52b2adea5f37065f6f1ac6732f28c0a
abcd40404 <abcd40404@yahoo.com.tw>
2019-04-19 18:35:29 +0800

    Update

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ openbox-session &
 sleep 2
 
 x11vnc -display :1 -forever -bg -ncache 10 -rfbport 5902 &
-cd /root/noVNC && ./utils/launch.sh --listen 6080 --vnc localhost:5902
+cd ~/noVNC && ./utils/launch.sh --listen 6080 --vnc localhost:5902
